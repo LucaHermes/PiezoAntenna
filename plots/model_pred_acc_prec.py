8b54d6e95b80824c3b470ab25a83a2373c4f3b8f
Luca <lhermes@techfak.uni-bielefeld.de>
2018-10-08 15:52:32 +0200

    add pots to show clipped contact events

---
+++ plots/model_pred_acc_prec.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import matplotlib as mlp
+import numpy as np
+
+#plt.figure(num=None, figsize=(15, 10), dpi=80)
+plt.style.use('ggplot')
+fig = plt.figure()
+
+gs = mlp.gridspec.GridSpec(2, 2)
+ax1 = plt.subplot(gs[:, :-1])
+ax2 = plt.subplot(gs[0, 1])
+ax3 = plt.subplot(gs[1, 1])
+
+model_scores = pd.read_csv('../data/analysis/model_scores.csv')
+model_scores = model_scores.dropna()
+
+f_hi = model_scores.loc[:, 'freqhi_actual']
+f_lo = model_scores.loc[:, 'freqlo_actual']
+r2 = model_scores.loc[:, 'r2']
+
+sorted_r2 = sorted(r2, reverse=True)
+sorted_f_hi = [ r for _, r in sorted(zip(r2, f_hi), reverse=True) ]
+sorted_f_lo = [ r for _, r in sorted(zip(r2, f_lo), reverse=True) ]
+
+TRAIN_ON = [sorted_f_lo[-1], sorted_f_hi[1]]
+
+
+
+exit(1)
+
+
+############################# remove singular rows of values that only occur once in column freqmid_actual
+#model_scores = model_scores[model_scores.groupby('freqmid_actual').freqmid_actual.transform(len) > 1]
+
+f_center = model_scores.loc[:, 'freqmid_actual']
+frac_width = (model_scores.loc[:, 'freqhi_actual'] - model_scores.loc[:, 'freqlo_actual']) / f_center
+r2 = model_scores.loc[:, 'r2']
+
+# find best performing
+sorted_r2 = [ r for _, r in sorted(zip(f_center, r2))]
+sorted_frac_width = [ r for _, r in sorted(zip(f_center, frac_width))]
+sorted_center = sorted(f_center)
+top = []
+highest = sorted_r2[0]
+xs = []
+for i in range(1, len(sorted_center)):
+    if sorted_center[i] == sorted_center[i-1]:
+        highest = max(highest, sorted_r2[i], sorted_r2[i-1])
+    else:
+        top.append(highest)
+        highest = sorted_r2[i]
+top.append(highest)
+
+p = plt.scatter(f_center, r2, marker='.', c=100*frac_width, cmap='rainbow')
+plt.plot(np.unique(sorted_center), top, color='k')
+plt.axhline(0.95, linestyle='--', color='k', linewidth=0.8)
+plt.xscale('log')
+ax.get_xaxis().set_major_formatter(ticker.ScalarFormatter())
+plt.xlabel('Center frequency, Hz')
+plt.ylabel(r'Coefficient of determination, $R^2$')
+
+cbar_ax = fig.add_axes([0.91, 0.1, 0.01, 0.5])
+cbar = fig.colorbar(p, cax=cbar_ax)
+cbar.set_label('Fractional bandwidth, %')
+
+
+
+
+"""
+
+frac_supp_vec = model_scores.loc[:, 'fsv']
+frac_supp_vec_sorted = [ r for _, r in sorted(zip(f_center, frac_supp_vec))]
+top = []
+highest = sorted_r2[0]
+f = frac_supp_vec_sorted[0]
+
+for i in range(1, len(sorted_center)):
+    if sorted_center[i] == sorted_center[i-1]:
+        if highest == max(highest, sorted_r2[i], sorted_r2[i-1]):
+            continue
+        if  sorted_r2[i] >= sorted_r2[i-1]:
+            f = frac_supp_vec_sorted[i]
+            highest = sorted_r2[i]
+        else:
+            f = frac_supp_vec_sorted[i-1]
+            highest = sorted_r2[i-1]
+    else:
+        top.append(f)
+        highest = 0
+        f = frac_supp_vec_sorted[i]
+
+top.append(highest)
+
+
+ax = plt.subplot(2, 1, 2)
+plt.scatter(f_center, frac_supp_vec, marker='.', c=100*frac_width, cmap='rainbow')
+plt.plot(np.unique(sorted_center), top, color='k')
+plt.xscale('log')
+ax.get_xaxis().set_major_formatter(ticker.ScalarFormatter())
+
+
+"""
+plt.show()
---
+++ plots/plot_clipped.py
@@ -0,0 +1,146 @@
+﻿# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import scipy.io.wavfile as wav
+import scipy.signal as signal
+import numpy as np
+import pandas as pd
+import os
+import re
+
+plt.style.use('ggplot')
+plt.subplots_adjust(hspace=.5)
+
+data_dir = '../data_clipped'
+files = os.listdir(data_dir)
+files = [ files[i] for i in range(4, 38, 8)]
+df = pd.DataFrame(columns=['record', 'distance', 'speed'])
+
+for f in files:
+    fs, data = wav.read(os.path.join(data_dir, f))
+    decoded_classes = [ int(s) for s in re.findall(r'-?\d+', f) ]
+    distance = decoded_classes[0]
+    speed = decoded_classes[1]
+
+    plt.subplot(len(files), 1, files.index(f)+1)
+    timeline = np.linspace(0, len(data) / float(fs), len(data))
+    plt.title('Distance %d cm - Speed %d $^\circ$/s' % (distance, speed))
+    plt.plot(timeline, data, color='k', linewidth=.5)
+    plt.xlim((2., 2.4))
+
+plt.show()
+"""
+
+def get_record(distance, speed):
+    
+    return df['record'][ (df['distance'] == distance) & (df['speed'] == speed) ].tolist()[0]
+
+
+for f in files:
+    if '.wav' not in f:
+        continue
+    fs, data = wav.read(os.path.join(data_dir, f))
+    decoded_classes = [ int(s) for s in re.findall(r'-?\d+', f) ]
+    # put the data into the DataFrame
+    df = df.append(dict(
+        { 'record'   : np.divide(data, float(np.max(data)))[:220500], 
+          'distance' : decoded_classes[0],
+          'speed'    : decoded_classes[1] }), ignore_index=True)
+    
+DISTANCES = sorted(df.distance.unique())
+SPEEDS = sorted(df.speed.unique())
+
+
+# ------------------- FIND PEAKS ----------------------------
+
+EXPECTED_CONTACTS = 100
+THRESHOLD = .6 #1844146018536
+FRAGMENT_SIZE = 2048
+SAMPLES_BETWEEN_CONTACTS = 8192
+
+def find_peaks(x, method='last_peaks'):
+    above_th = pd.Series(x) > THRESHOLD
+    # find every threshold transgression
+    above_th = pd.Series(above_th.index[above_th == True].tolist()).astype(int)
+    diff = above_th.diff(periods=-1).abs()
+    if method == 'first_peaks':
+        diff = diff.shift(1).fillna(SAMPLES_BETWEEN_CONTACTS)
+    peaks_indices = diff.index[diff >= SAMPLES_BETWEEN_CONTACTS].tolist()
+    peaks_indices.append(len(above_th) - 1)
+    peaks = above_th[peaks_indices].reset_index(drop=True)
+    return peaks
+
+def extract_peaks(d, peaks):
+    idx_peaks = pd.Index(peaks)
+    contacts = []
+    for c_idx in peaks:
+        contacts.append(d[c_idx:c_idx + FRAGMENT_SIZE].tolist())
+    return peaks, contacts
+
+
+contact_signals = pd.DataFrame()
+
+for i in range(len(df)):
+    speed = df.at[i, 'speed']
+    distance = df.at[i, 'distance']
+    d = pd.Series(df.at[i, 'record'])
+    peaks = find_peaks(d.abs())
+    if len(peaks) != EXPECTED_CONTACTS:
+        print('Unexpected amount of contacts (%d) in set distance %d - speed %d' 
+              % (len(peaks), distance, speed))
+    x_ix, contacts = extract_peaks(d, peaks)
+    contact_signals = contact_signals.append([[speed, distance, x_ix] + contacts], ignore_index=True)
+    
+contact_signals.rename(columns={ 0 : 'speed', 1 : 'distance', 2 : 'x_ix' }, inplace=True)
+last_col = contact_signals.columns[-1]
+contact_signals.rename(columns=dict(zip(range(3, last_col), range(0, last_col-2))), inplace=True)
+contact_signals.rename(columns={ 101 : 99 }, inplace=True)
+cs = contact_signals.iloc[:, :6]
+
+# -------------------- PLOT DATA ----------------------------
+
+plt.figure(num=None, figsize=(15, 8), dpi=80)
+plt.subplots_adjust(wspace=0.1)
+plt.style.use('ggplot')
+
+PLT_DISTANCE = 15
+PLT_SPEED = 100
+plot_at = 1
+
+# plot for varying distance
+for distance in range(5, 24, 2):
+    # plot for varying speed
+    for speed in range(60, 190, 20):
+    	plt.subplot(10, 7, plot_at)
+        if distance == 23:
+            plt.xlabel("Time, sec")
+        else:
+            plt.xticks(np.arange(0, 5, 0.2), ' ')
+        if distance == 5:
+            plt.title('%d $^\circ$/s' % speed)
+        if speed == 60:
+            plt.ylabel('%d cm' % distance, fontsize=15, labelpad=25, rotation=0)
+        else:
+            plt.yticks([-1, 0, 1], ' ')
+
+        data = get_record(distance, speed)
+        #print( cs[cs.distance == distance][cs.speed == speed])
+        fragm = cs[cs.distance == distance][cs.speed == speed].iloc[0, 2:]
+        x_ix, cf = fragm['x_ix'][1], fragm[1]
+        timeline = np.linspace(0, len(data) / 44100., len(data))
+        frag_time = np.linspace(x_ix / 44100., (x_ix + len(cf)) / 44100., len(cf))
+        plt.plot(frag_time, cf, color='k')
+        plt.plot(timeline, data, color='k', linewidth=.5, alpha=.5)
+        plt.plot()
+        #plt.xlim((2.1, 2.3))
+        center_t = frag_time[0] + 0.5*(frag_time[-1] - frag_time[0])
+        print(center_t)
+        print(frag_time)
+        low_t = center_t - 0.08
+        hi_t = center_t + 0.08
+        plt.xlim((low_t, hi_t))
+
+        plt.ylim((-1, 1))
+        plot_at += 1
+
+plt.show()
+"""
